d6419fe419c05d31c26b6959f3b97b88fd84ef39
Basil Baby <basilbabydzire@gmail.com>
2020-09-26 22:00:07 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-        "io/ioutil"
-        "log"
-        "net/http"
-        "path/filepath"
+	"io/ioutil"
+	"log"
+	"net/http"
+    "path/filepath"
+    "os"
 )
 
 // GeoJSON is a cache of the NYC Subway Station and Line data.
@@ -12,30 +13,48 @@ var GeoJSON = make(map[string][]byte)
 
 // cacheGeoJSON loads files under data into `GeoJSON`.
 func cacheGeoJSON() {
-        filenames, err := filepath.Glob("data/*")
-        if err != nil {
-                // Note: this will take down the GAE instance by exiting this process.
-                log.Fatal(err)
-        }
-        for _, f := range filenames {
-                name := filepath.Base(f)
-                dat, err := ioutil.ReadFile(f)
-                if err != nil {
-                        log.Fatal(err)
-                }
-                GeoJSON[name] = dat
-        }
+	filenames, err := filepath.Glob("data/*")
+	if err != nil {
+		// Note: this will take down the GAE instance by exiting this process.
+		log.Fatal(err)
+	}
+	for _, f := range filenames {
+		name := filepath.Base(f)
+		dat, err := ioutil.ReadFile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		GeoJSON[name] = dat
+	}
 }
 
-// init is called from the App Engine runtime to initialize the app.
-func init() {
-        cacheGeoJSON()
-        loadStations()
-        http.HandleFunc("/data/subway-stations", subwayStationsHandler)
-        http.HandleFunc("/data/subway-lines", subwayLinesHandler)
+func main() {
+    cacheGeoJSON()
+	loadStations()
+	http.HandleFunc("/data/subway-stations", subwayStationsHandler)
+    http.HandleFunc("/data/subway-lines", subwayLinesHandler)
+    http.HandleFunc("/", indexHandler)
+
+    port := os.Getenv("PORT")
+    if port == "" {
+            port = "8080"
+            log.Printf("Defaulting to port %s", port)
+    }
+
+    log.Printf("Listening on port %s", port)
+    if err := http.ListenAndServe(":"+port, nil); err != nil {
+            log.Fatal(err)
+    }
 }
 
 func subwayLinesHandler(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-type", "application/json")
-        w.Write(GeoJSON["subway-lines.geojson"])
+	w.Header().Set("Content-type", "application/json")
+	w.Write(GeoJSON["subway-lines.geojson"])
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+            http.NotFound(w, r)
+            return
+    }
 }
